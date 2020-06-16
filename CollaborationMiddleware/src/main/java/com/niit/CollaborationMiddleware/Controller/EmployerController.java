576e9cc31545eaa378b8a9840ed84ccd2c5b0775
sandhiya74 <sandhyasandhu1079@gmail.com>
2020-06-16 13:15:33 +0530

    Employer controller
    update

--- CollaborationMiddleware/src/main/java/com/niit/CollaborationMiddleware/Controller/EmployerController.java
+++ CollaborationMiddleware/src/main/java/com/niit/CollaborationMiddleware/Controller/EmployerController.java
@@ -1,6 +1,34 @@
 package com.niit.CollaborationMiddleware.Controller;
 
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.niit.Collaboration.DAO.ICustomerDAO;
+import com.niit.Collaboration.Model.Customer;
+
+@RestController
 public class EmployerController {
 
+	@Autowired
+	ICustomerDAO customerdao;
+	
+	ResponseEntity<List<Customer>> selectAllCustomer()
+	{
+		ArrayList<Customer> custlist=(ArrayList<Customer>)customerdao.selectAllCustomer();
+		if(custlist.isEmpty())
+		{
+			return new ResponseEntity<List<Customer>>(HttpStatus.NOT_FOUND);
+		}
+		else
+		{
+		return new ResponseEntity<List<Customer>>(custlist,HttpStatus.FOUND);
+		}
+		
+	}
+	   
 }
